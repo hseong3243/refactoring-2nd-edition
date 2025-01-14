0e3a106a1fbe534a9ab4dd814f6c05ae6549ca98
hseong3243 <hseong3243@gmail.com>
2025-01-14 22:45:24 +0900

    1장 예제 함수 작성

--- src/main/kotlin/org/seong/refactoring2ndedition/Refactoring2ndEditionApplication.kt
+++ src/main/kotlin/org/seong/refactoring2ndedition/Refactoring2ndEditionApplication.kt
@@ -2,16 +2,54 @@ package org.seong.refactoring2ndedition
 
 import org.seong.refactoring2ndedition.dto.Invoice
 import org.seong.refactoring2ndedition.dto.Play
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
-@SpringBootApplication
 class Refactoring2ndEditionApplication
 
-fun main(args: Array<String>) {
-    runApplication<Refactoring2ndEditionApplication>(*args)
+fun main() {
+
+
+
 }
 
-fun statement(invoice: Invoice, plays: Map<String, Play>) {
-    
+fun statement(invoice: Invoice, plays: Map<String, Play>): String {
+    var totalAmount = 0
+    var volumeCredits = 0
+    var result = "청구 내역 (고객명: ${invoice.customer}\n"
+
+    for (perf in invoice.performances) {
+        val play = plays.get(perf.playID)
+        var thisAmount = 0
+
+        when (play!!.type) {
+            "tragedy" -> { // 비극
+                thisAmount = 40000
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30)
+                }
+            }
+
+            "comedy" -> { // 희극
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience
+            }
+            else -> {
+                throw Exception("알 수 없는 장르: ${play.type}")
+            }
+        }
+
+        // 포인트를 적립한다.
+        volumeCredits += Math.max(perf.audience - 30, 0)
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if ("comedy" == play.type) volumeCredits += Math.floor((perf.audience / 5).toDouble()).toInt();
+
+        // 청구 내역을 출력한다.
+        result += " ${play.name}: ${thisAmount/100} (${perf.audience}석)\n"
+        totalAmount += thisAmount
+    }
+    result += "총액: ${totalAmount/100}\n"
+    result += "적립 포인트: ${volumeCredits}점\n"
+    return result;
 }
