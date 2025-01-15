4d7d18cd912fc50a139ac34079b1438c394f5f0f
hseong3243 <hseong3243@gmail.com>
2025-01-15 21:37:50 +0900

    1장 예제 입력 데이터 추가

--- src/main/kotlin/org/seong/refactoring2ndedition/dto/Invoice.kt
+++ src/main/kotlin/org/seong/refactoring2ndedition/dto/Invoice.kt
@@ -1,8 +1,39 @@
 package org.seong.refactoring2ndedition.dto
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
 data class Invoice(
     val customer: String,
     val performances: List<Performance>
 )
 
-data class Performance(val playId: String, val audience: Int)
+@Serializable
+data class Performance(val playID: String, val audience: Int)
+
+fun getInvoices(): List<Invoice> {
+    return Json.decodeFromString<List<Invoice>>(invoicesJson)
+}
+
+private const val invoicesJson = """
+        [
+            {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "hamlet",
+                        "audience": 55
+                    },
+                    {
+                        "playID": "as-like",
+                        "audience": 35
+                    },
+                    {
+                        "playID": "othello",
+                        "audience": 40
+                    }
+                ]
+            }
+        ]
+    """
--- src/main/kotlin/org/seong/refactoring2ndedition/dto/Play.kt
+++ src/main/kotlin/org/seong/refactoring2ndedition/dto/Play.kt
@@ -1,8 +1,22 @@
 package org.seong.refactoring2ndedition.dto
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
 data class Play(
     val name: String,
     val type: String
-) {
+)
 
+fun getPlayMap(): Map<String, Play> {
+    return Json.decodeFromString<Map<String, Play>>(playsJson)
 }
+
+private const val playsJson = """
+        {
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        }
+    """
