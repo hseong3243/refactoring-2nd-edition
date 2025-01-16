082252383b06f5b74c128afae0934e217c4d1e8c
hseong3243 <hseong3243@gmail.com>
2025-01-16 23:01:34 +0900

    refactor 지역 변수 제거
    함수로 추출하여 지역 변수를 제거함

--- src/main/kotlin/org/seong/refactoring2ndedition/Application.kt
+++ src/main/kotlin/org/seong/refactoring2ndedition/Application.kt
@@ -15,6 +15,36 @@ fun main() {
 }
 
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
+    fun playFor(aPerformance: Performance): Play? {
+        return plays[aPerformance.playID]
+    }
+    fun amountFor(
+        aPerformance: Performance
+    ): Int {
+        var result = 0
+        when (playFor(aPerformance)?.type) {
+            "tragedy" -> { // 비극
+                result = 40000
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+            }
+
+            "comedy" -> { // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience
+            }
+
+            else -> {
+                throw Exception("알 수 없는 장르: ${playFor(aPerformance)?.type}")
+            }
+        }
+        return result
+    }
+
     var totalAmount = 0
     var volumeCredits = 0
     var result = "청구 내역 (고객명: ${invoice.customer})\n"
@@ -23,48 +53,17 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
     }
 
     for (perf in invoice.performances) {
-        val play = plays.get(perf.playID)
-        val thisAmount = amountFor(perf, play)
-
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0)
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" == play?.type) volumeCredits += Math.floor((perf.audience / 5).toDouble())
+        if ("comedy" == playFor(perf)?.type) volumeCredits += Math.floor((perf.audience / 5).toDouble())
             .toInt();
 
         // 청구 내역을 출력한다.
-        result += " ${play?.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n"
-        totalAmount += thisAmount
+        result += " ${playFor(perf)?.name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n"
+        totalAmount += amountFor(perf)
     }
     result += "총액: ${format(totalAmount / 100)}\n"
     result += "적립 포인트: ${volumeCredits}점\n"
     return result;
-}
-
-private fun amountFor(
-    aPerformance: Performance,
-    play: Play?
-): Int {
-    var result = 0
-    when (play!!.type) {
-        "tragedy" -> { // 비극
-            result = 40000
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30)
-            }
-        }
-
-        "comedy" -> { // 희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience
-        }
-
-        else -> {
-            throw Exception("알 수 없는 장르: ${play.type}")
-        }
-    }
-    return result
 }
