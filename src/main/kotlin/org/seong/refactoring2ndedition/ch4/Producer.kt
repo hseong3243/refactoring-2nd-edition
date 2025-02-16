cea7c5d06b9b6b4c1bd51cd7c1c908546a4527b8
hseong3243 <hseong3243@gmail.com>
2025-02-16 21:52:58 +0900

    4장 feat Producer 클래스 추가

---
+++ src/main/kotlin/org/seong/refactoring2ndedition/ch4/Producer.kt
@@ -0,0 +1,7 @@
+package org.seong.refactoring2ndedition.ch4
+
+class Producer(
+    val name: String,
+    val cost: Int,
+    val production: Int
+)
---
+++ src/main/kotlin/org/seong/refactoring2ndedition/ch4/Province.kt
@@ -0,0 +1,9 @@
+package org.seong.refactoring2ndedition.ch4
+
+class Province(
+    val name: String,
+    val producers: List<Producer>,
+    val totalProduction: Int,
+    val demand: String,
+    val price: Int,
+)
