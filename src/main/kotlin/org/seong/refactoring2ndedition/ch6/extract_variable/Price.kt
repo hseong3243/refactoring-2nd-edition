c40888b0e96613fde1642ed13cd4274efd0de7cf
hseong3243 <hseong3243@gmail.com>
2025-02-26 22:37:21 +0900

    6.3 feat 변수 추출 예시 함수 price 추가

---
+++ src/main/kotlin/org/seong/refactoring2ndedition/ch6/extract_variable/Price.kt
@@ -0,0 +1,19 @@
+package org.seong.refactoring2ndedition.ch6.extract_variable
+
+import kotlin.math.max
+import kotlin.math.min
+
+class Price {
+
+    fun price(order: Order) : Double {
+        // 가격(price) = 기본 가격 - 수량 할인 + 배송비
+        return order.quantity * order.itemPrice -
+                max(0, order.quantity-500) * order.itemPrice * 0.05 +
+                min(order.quantity * order.itemPrice * 0.1, 100.0)
+    }
+}
+
+data class Order(
+    val quantity: Int,
+    val itemPrice: Int,
+)
