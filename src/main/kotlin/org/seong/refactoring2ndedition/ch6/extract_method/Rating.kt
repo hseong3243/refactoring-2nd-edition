3e43ed07f58ec019f0e3da07ad4001ed51c18cd7
hseong3243 <hseong3243@gmail.com>
2025-02-26 22:16:48 +0900

    6.2 refactor 메서드 인라인

--- src/main/kotlin/org/seong/refactoring2ndedition/ch6/extract_method/Rating.kt
+++ src/main/kotlin/org/seong/refactoring2ndedition/ch6/extract_method/Rating.kt
@@ -1,14 +1,9 @@
 package org.seong.refactoring2ndedition.ch6.extract_method
 
-
 class Rating {
 
     fun rating(aDriver: Driver): Int {
-        return if (moreThanFiveLateDeliveries(aDriver)) 2 else 1
-    }
-
-    private fun moreThanFiveLateDeliveries(aDriver: Driver): Boolean {
-        return aDriver.numberOfLateDeliveries > 5;
+        return if (aDriver.numberOfLateDeliveries > 5) 2 else 1
     }
 }
 
